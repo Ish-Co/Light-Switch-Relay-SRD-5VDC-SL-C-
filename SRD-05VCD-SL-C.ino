02e3e4454dd474c6aeee39bba211a5509897a125
Ismail Sghir <73454465+Ish-Co@users.noreply.github.com>
2020-12-28 15:08:40 -0500

    Create SRD-05VCD-SL-C.ino

---
+++ SRD-05VCD-SL-C.ino
@@ -0,0 +1,9 @@
+//----------------------------
+//Author      : Ismail Sghir
+//Last updated: 12/28/2020
+//----------------------------
+
+int ledPin = 6;             // LED pin
+int trigPin = 7;            // PIR pin 
+
+
